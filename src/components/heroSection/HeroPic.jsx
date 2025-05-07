c58beb8d71c446e9467a9395fb55e0a8757be56f
DevFeReactjs <quankontum55555@gmail.com>
2025-05-08 02:00:03 +0700

    Fix image path for Vercel deployment

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -4,10 +4,11 @@ const HeroPic = () => {
     return (
         <div className=" h-full flex items-center justify-center ">
             <img
-                src="../../../public/images/ImagesMQ.png"
+                src="/images/ImagesMQ.png"
                 alt="Thai Tran Minh Quan"
                 className="max-h-[450px] w-auto"
             />
+
             <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
                 <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
             </div>
