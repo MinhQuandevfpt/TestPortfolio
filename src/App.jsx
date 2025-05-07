26699ce9762ed53c23a438096aebba5f5750a794
DevFeReactjs <quankontum55555@gmail.com>
2025-05-08 01:40:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import AboutMeMain from './components/aboutMeSection/AboutMeMain'
+import HelperSection from './components/HelperSection'
+import HeroGradient from './components/heroSection/HeroGradient'
+import HeroMain from './components/heroSection/HeroMain'
+import SubHeroSection from './components/heroSection/SubHeroSection'
+import NavbarMain from './components/navbar/NavbarMain'
+
+
+
+function App() {
+  return <main className='font-body'> 
+    <NavbarMain />
+    <HeroMain />
+    <HeroGradient />
+    <SubHeroSection />
+    <AboutMeMain />
+    <HelperSection />
+  
+  </main>
+
+}
+
+export default App
---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,15 @@
+const NavbarLogo = () => {
+    return (
+      <div>
+        <h1 className="text-white  text-2xl sm:hidden md:block">
+          QuanTTM
+        </h1>
+        <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+          MQ
+        </h1>
+      </div>
+    );
+  };
+  
+  export default NavbarLogo;
+  
